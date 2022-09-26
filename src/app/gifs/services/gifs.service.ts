7789f1d2a954c34144ec7b3bd27e8462febc679a
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-09-25 21:09:02 -0500

    REALIZANDO LA PETICION HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -12,13 +13,40 @@ export class GifsService {
     return [...this._historial];
   }
 
-  buscarGifts(query: string = '') {
+  constructor(private http: HttpClient) {}
+
+  /* async */ buscarGifts(query: string = '') {
     query = query.trim().toLowerCase();
 
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
     }
-    console.log(this._historial);
+    /* CON FETCH JS */
+    // fetch(
+    //   'https://api.giphy.com/v1/gifs/search?api_key=B3gI67y5Y8n4paPJIpZQ04g2HdwcG5zm&q=dragon ball z&limit=10'
+    // ).then((resp) => {
+    //   resp.json().then((data) => {
+    //     console.log(data);
+    //   });
+    // });
+
+    /* CON ASYNC AWAIT */
+    // const resp = await fetch(
+    //   'https://api.giphy.com/v1/gifs/search?api_key=B3gI67y5Y8n4paPJIpZQ04g2HdwcG5zm&q=dragon ball z&limit=10'
+    // );
+
+    // const data = await resp.json();
+    // console.log(data);
+
+    /* CON HTTP CLIENT -> FROM @angular/common/http */
+
+    this.http
+      .get(
+        'https://api.giphy.com/v1/gifs/search?api_key=B3gI67y5Y8n4paPJIpZQ04g2HdwcG5zm&q=dragon ball z&limit=10'
+      )
+      .subscribe((resp:any) => {
+        console.log(resp.data);
+      });
   }
 }
